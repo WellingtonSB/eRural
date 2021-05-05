df8b985ddd19bfbf9cc8eae7d445d003fc596269
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-05-05 14:18:56 -0300

    service-entrar

--- FrontEnd/eRural/src/app/model/UserLogin.ts
+++ FrontEnd/eRural/src/app/model/UserLogin.ts
@@ -1,9 +1,11 @@
 
 export class UserLogin {
+
     public id : number
     public nome :string
     public usuario: string
     public tipoUsuario:string
     public senha :string
     public token: string
+    
 }
--- FrontEnd/eRural/src/app/service/auth.service.ts
+++ FrontEnd/eRural/src/app/service/auth.service.ts
@@ -1,12 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  serverPort = environment.server + environment.port
+
   constructor(
     private Http:HttpClient
   ) { }
+
+    entrar(userLogin:UserLogin): Observable<UserLogin>{
+      return this.Http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    }
+
+   
+
+   
+
+
+
+
+
+
+
 }
